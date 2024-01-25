9f415c0c83e809bc3392f7c7af05aef95eefba72
Preston <71202878+preston-k@users.noreply.github.com>
2024-01-24 22:07:04 -0800

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ function track() {
     } else if (linkid === 'food') {
       database.ref('path/' + 'food' + '/linkNickname').set('prestonkwei.com/comingsoon.html');
       updateClickCount('VA6B640NP9')   
-      window.location.replace('https://prestonkwei.com/links.html')
+      // window.location.replace('https://prestonkwei.com/links.html')
     } 
   } else if (linkid.length == 36) {
     // QR CODE SEQUENCE:
