1dab89920affb37ae0c1be4485dc9cc450594264
Preston Kwei <prestonkwei@gmail.com>
2024-12-29 20:12:46 -0800

    change email and styling

--- tracking.js
+++ tracking.js
@@ -1,15 +1,15 @@
 /** @type {typeof import('./static.json')} */
-const data = await fetch('/static.json').then((x) => x.json());
+const data = await fetch('/static.json').then((x) => x.json())
 
-const version = data.version;
+const version = data.version
 function init() {
-  console.log('LinkTrack (go.prestonkwei.com)');
-  console.log(data);
+  console.log('LinkTrack (go.prestonkwei.com)')
+  console.log(data)
 }
-init();
+init()
 function modal() {
-  let insidep = document.querySelector('#redirect');
-  insidep.style.display = 'none';
+  let insidep = document.querySelector('#redirect')
+  insidep.style.display = 'none'
   let myModal = new bootstrap.Modal(document.querySelector('#staticBackdrop'), {
     keyboard: false,
   })
@@ -18,18 +18,18 @@ function modal() {
 import firebaseConfig from './firebase.js'
 firebase.initializeApp(firebaseConfig)
 
-let database = firebase.database();
+let database = firebase.database()
 let linkid = new URLSearchParams(window.location.search).get('id')
-let ip 
+let ip
 let created = false
 fetch('https://api.ipify.org')
-  .then(res => res.text())
-  .then(data => {
+  .then((res) => res.text())
+  .then((data) => {
     ip = data
-})
+  })
 async function updateCount(id, current) {
   database.ref(`/${id}/`).update({
-    count: current += 1
+    count: (current += 1),
   })
   let countId = self.crypto.randomUUID()
   let ts = Date.now()
@@ -37,26 +37,30 @@ async function updateCount(id, current) {
     id: countId,
     ts: ts,
     exact: new Date(),
-    ip: ip
+    ip: ip,
   })
 }
 console.log(linkid)
 if (linkid == '' || linkid == null) {
   modal()
 } else {
-  database.ref(`/${linkid}/`).once('value').then(async (snapshot) => {
-    console.log(snapshot.val())
-    let data = snapshot.val()
-    console.log(data.count)
-    await updateCount(linkid, data.count)
-    const email = new FormData()
-    email.set('sendto', 'prestonkwei@gmail.com')
-    email.set('subject', 'Your link has been clicked!')
-    email.set('content', `Hi!\n\nLink Target: ${data.target}\n\nts: ${new Date()}\n\nip: ${ip}\n\n-----\ncurrent: ${data.count} / new: ${data.count + 1}\n\nhttps://go.prestonkwei.com/?id=${linkid}`)
-    await fetch('https://emailserver.prestonkwei.com/email', {
-      method: 'post',
-      body: email,
-    }).catch(() => {})
-    window.location.replace(data.target)
-  })
+  database
+    .ref(`/${linkid}/`)
+    .once('value')
+    .then(async (snapshot) => {
+      console.log(snapshot.val())
+      let data = snapshot.val()
+      console.log(data.count)
+      await updateCount(linkid, data.count)
+      const email = new FormData()
+      email.set('sendto', 'prestonkwei@gmail.com')
+      email.set('from', 'linktrack-noreply@e.prestonkwei.com')
+      email.set('subject', 'Your link has been clicked!')
+      email.set('content', `Hi!\n\nLink Target: ${data.target}\n\nts: ${new Date()}\n\nip: ${ip}\n\n-----\ncurrent: ${data.count} / new: ${data.count + 1}\n\nhttps://go.prestonkwei.com/?id=${linkid}`)
+      await fetch('https://emailserver.prestonkwei.com/email', {
+        method: 'post',
+        body: email,
+      }).catch(() => {})
+      window.location.replace(data.target)
+    })
 }
